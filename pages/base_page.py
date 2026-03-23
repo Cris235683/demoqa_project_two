55928b84f15d043fd3dac7500c334b6efac09d41
bondarova <listiksona18@gmail.com>
2026-03-23 12:47:44 +0300

    commit

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,3 +4,10 @@ class BasePage():
 
     def open(self, url):
         self.driver.get(url)
+
+class MainPage(BasePage):
+    def __init__(self, driver):
+        self.driver = driver
+
+    def open(self, url):
+        self.driver.get(url)
